94c619520da1d94983d802a092cc46e5d9b0561f
vaibhav290105 <vaibhavgupta290105@gmail.com>
2025-01-14 15:19:34 +0530

    3

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:['https://chat-2-swart.vercel.app/login'],
+        origin:['https://chat-2-swart.vercel.app'],
         methods:['GET', 'POST'],
     },
 });
